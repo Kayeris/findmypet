e01ecafa463b006bd9cf3706a42cf2cff1b65d41
janusanb <janusanb@gmail.com>
2021-05-02 11:30:49 -0400

    added the animal type given the breed

--- predicting.py
+++ predicting.py
@@ -1,12 +1,14 @@
 # Requirements: fastbook, import PIL, from PIL import Image, import requests, from io import BytesIO, import numpy, 
 import fastbook
 from fastbook import *
-# from fastai.basic_train import load_learner
 # import fastai
-# from fastai.vision import *
+import urllib
+# from fastai.vision import * 
+# from fastai.basic_train import load_learner
 
 def load_model(the_path):
   from pathlib import Path
+
   url = "https://storage.googleapis.com/ml_model_py/export.pkl"
   urllib.request.urlretrieve(url, '/tmp/export.pkl')
 
@@ -26,6 +28,7 @@ def prediction(image_file, the_path):
   import numpy
 
   import json
+  data = {}
   breed_classify = load_model(the_path)
 
   url = image_file
@@ -34,10 +37,15 @@ def prediction(image_file, the_path):
 
   pix = numpy.array(imgg)
 
-  what,x,tensor = breed_classify.predict(pix)
+  what,_,tensor = breed_classify.predict(pix)
+  if what.lower() in ['abyssinian', 'bengal', 'birman', 'bombay', 'british_shorthair', 'egyptian_mau', 'maine_coon', 'persian', 'ragdoll', 'russian_blue', 'siamese', 'sphynx']:
+    data['animal'] = ['cat']
+  
+  if what.lower() in ['american_bulldog', 'american_pit_bull_terrier', 'basset_hound', 'beagle', 'boxer', 'chihuahua', 'english_cocker_spaniel', 'english_setter', 'german_shorthaired', 'great_pyrenees', 'havanese', 'japanese_chin', 'keeshond', 'leonberger', 'miniature_pinscher', 'newfoundland', 'pomeranian', 'pug', 'saint_bernard', 'samoyed', 'scottish_terrier', 'shiba_inu', 'staffordshire_bull_terrier', 'wheaten_terrier', 'yorkshire_terrier']:
+    data['animal'] = ['cat']
+  
+  data['prediction'] = [what.lower()]
 
-  data = {}
-  data['prediction'] = [what]
 
   with open('data.txt', 'w'): # as outfile:
     json.dumps(data)
